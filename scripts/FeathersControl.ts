d2a541ed6e0f629082cb6f5bc6195c2f4835f586
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-12-12 21:38:12 +0800

    FeathersControl.ts (almost)

--- scripts/FeathersControl.ts
+++ scripts/FeathersControl.ts
@@ -1,6 +1,7 @@
 import TouchGestures from 'TouchGestures';
 import Scene from 'Scene';
 import Reactive from 'Reactive';
+import Diagnostics from 'Diagnostics';
 
 import {
     Wait,
@@ -12,18 +13,21 @@ import {
     const feathers: ParticleSystem = await Scene.root.findFirst('Feathers') as ParticleSystem;
     const rect: SceneObjectBase = await Scene.root.findFirst('Rect') as SceneObjectBase;
 
-    feathers.hidden = Reactive.val(true);
+    feathers.birthrate = Reactive.val(0);
 
     function* MyRoutine(): IterableIterator<Wait> {
-        feathers.hidden = Reactive.val(false);
+        feathers.birthrate = Reactive.val(100);
 
         yield new WaitForMilliseconds(5700);
 
-        feathers.hidden = Reactive.val(true);
+        feathers.birthrate = Reactive.val(0);
     }
 
     const touchSub: Subscription = TouchGestures.onTap(rect).subscribe((event: TapGesture): void => {
-        if(feathers.hidden.pinLastValue) {
+        Diagnostics.log("here");
+
+        if(feathers.birthrate.eq(0.0)) {
+            Diagnostics.log("hereInside");
             StartCoroutine(MyRoutine);
         }
     });
