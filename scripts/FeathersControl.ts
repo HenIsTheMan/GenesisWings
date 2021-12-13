2b95f495e3fbc77faf9f68bf51f11150331fd006
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-12-13 09:43:57 +0800

    FeatherParticleSystem

--- scripts/FeathersControl.ts
+++ scripts/FeathersControl.ts
@@ -1,7 +1,6 @@
 import TouchGestures from 'TouchGestures';
 import Scene from 'Scene';
 import Reactive from 'Reactive';
-import Diagnostics from 'Diagnostics';
 
 import {
     Wait,
@@ -10,24 +9,21 @@ import {
 } from './Coroutine'
 
 (async function(): Promise<void> {
-    const feathers: ParticleSystem = await Scene.root.findFirst('Feathers') as ParticleSystem;
+    const featherParticleSystem: ParticleSystem = await Scene.root.findFirst('FeatherParticleSystem') as ParticleSystem;
     const rect: SceneObjectBase = await Scene.root.findFirst('Rect') as SceneObjectBase;
 
-    feathers.birthrate = Reactive.val(0.0);
+    featherParticleSystem.birthrate = Reactive.val(0.0);
 
     function* MyRoutine(): IterableIterator<Wait> {
-        feathers.birthrate = Reactive.val(100.0);
+        featherParticleSystem.birthrate = Reactive.val(100.0);
 
         yield new WaitForMilliseconds(4000);
 
-        feathers.birthrate = Reactive.val(0.0);
+        featherParticleSystem.birthrate = Reactive.val(0.0);
     }
 
     const touchSub: Subscription = TouchGestures.onTap(rect).subscribe((event: TapGesture): void => {
-        Diagnostics.log("here");
-
-        if(feathers.birthrate.pinLastValue() == 0.0) {
-            Diagnostics.log("hereInside");
+        if(featherParticleSystem.birthrate.pinLastValue() == 0.0) {
             StartCoroutine(MyRoutine);
         }
     });
