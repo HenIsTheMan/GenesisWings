921025426e7cd583c870899d9cc4f7333c97be12
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-12-13 12:35:02 +0800

    optionSelectedSub (works!!)
    - https://sparkar.facebook.com/ar-studio/learn/patch-editor/bridging/
    - https://sparkar.facebook.com/ar-studio/learn/scripting/reactive/

--- scripts/FeathersControl.ts
+++ scripts/FeathersControl.ts
@@ -1,6 +1,8 @@
 import TouchGestures from 'TouchGestures';
 import Scene from 'Scene';
 import Reactive from 'Reactive';
+import Patches from 'Patches';
+import Diagnostics from 'Diagnostics';
 
 import {
     Wait,
@@ -13,6 +15,21 @@ import {
     const rect: SceneObjectBase = await Scene.root.findFirst('Rect') as SceneObjectBase;
 
     featherParticleSystem.birthrate = Reactive.val(0.0);
+
+    const selectedOptionIndexSignal: ScalarSignal = await Patches.outputs.getScalar('selectedOptionIndex');
+    const optionSelectedSub: Subscription = selectedOptionIndexSignal.monitor().subscribe(function (event: {
+        newValue: number;
+        oldValue: number;
+    }): void {
+        switch(event.newValue) {
+            case 0:
+                Diagnostics.log(event.newValue);
+                break;
+            case 1:
+                Diagnostics.log(event.newValue);
+                break;
+        }
+    });
 
     function* MyRoutine(): IterableIterator<Wait> {
         featherParticleSystem.birthrate = Reactive.val(100.0);
